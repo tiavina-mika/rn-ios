3999d2714f8714c13d723d29acb56f591b21dcf2
tiavina-mika <ralainirinatiavina@gmail.com>
2021-12-23 17:13:57 +0300

    change message

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello there!</Text>
+      <Text>Hello there Mr Krabs!</Text>
       <StatusBar style="auto" />
     </View>
   );
