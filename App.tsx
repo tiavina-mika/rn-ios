ab9f306d4cbfecda1b5bbba147bc565d42e76666
tiavina-mika <ralainirinatiavina@gmail.com>
2021-12-24 11:15:48 +0300

    downgrade

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
+import React from 'react';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello there Mr Krabs!</Text>
+      <Text>Hello there Mr Tiks!</Text>
       <StatusBar style="auto" />
     </View>
   );
